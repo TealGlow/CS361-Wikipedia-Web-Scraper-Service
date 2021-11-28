e7978a0323e4fac307a3303b26e2274d24c2115e
TealGlow <alyssacomstock95@gmail.com>
2021-11-27 16:06:20 -0800

    initial commit on public version of repo

---
+++ README.md
@@ -0,0 +1,120 @@
+# WikiWebScraper
+
+Name: Alyssa Comstock
+
+Date: 11/27/2021
+
+Class: CS 361 - Software Engineering I
+
+Purpose: Micro-service
+
+Description: RESTFUL API built in Node.js, Express.js, and other packages. On GET request the user will back a json
+object containing the information from 2 API calls to a weather API, and the data from the Wikipeida article based on
+the zip code from the GET request parameter.  The wikipedia data is cleaned, and split into strings based on the paragraph
+tags in the HTML returned to the web scraper.
+
+Note this is the public version of this git repo.
+
+
+## Table of contents
+- [Description](#wikiwebscraper)
+- [How to Install](#how-to-install)
+- [How to use](#how-to-use)
+- [Example Ouput](#example-output)
+- [Packages used](#Packages-used)
+
+
+## How to install
+
+requirements:
+- [npm](https://www.npmjs.com/)
+- [Node.js](https://nodejs.org/en/download/)
+
+Installing:
+1. Go to the directory that you wish to place this program
+2. Place my program there:
+```
+For command line:
+git clone https://github.com/TealGlow/WikiWebScraper.git
+```
+3. Open a terminal in the location of the app.js file:
+
+4. install node modules / dependencies:
+```
+npm install
+```
+5. Run the program!
+```
+node app.js
+```
+6. (Optional) If you make any edits to the file, you can use nodemon to auto-rerun the program when changes are saved. Very nice if you need to change the port:
+```
+nodemon app.js
+```
+
+
+If it is running correctly it should output in the command line that it is running on localhost with the port specified.
+
+
+## How to Use:
+Make sure that the app.js file is running, you can change the port.
+Make a GET request to the http:localhost:3000/location path.  Currently does not support POST requests.
+
+Please note: this is using Chris's zip code validator, so that needs to be running at the same time.
+
+
+Example:
+```
+http://localhost:3000/location?zip=97008
+```
+
+## Example how to request with Axios
+
+Axios will return a promise from my API. This is obviously not the only way but as long as you make GET request to that path with the
+corret format you should get the data back in JSON format.
+
+```
+const axios = require('axios');
+
+
+// based on the axios example in their docs
+// https://github.com/axios/axios
+
+const data_request = axios.get("http://localhost:3000/location?zip=97008")
+  .then((data)=>{
+    // do whatever you want with this data
+  }.catch((error)=>{
+    // handle error
+  });
+```
+
+
+## Example Output
+
+Please read the documentation for the format of the [current](https://openweathermap.org/current) and [five day](https://openweathermap.org/forecast5) forecasts.
+
+notes:
+- The units will be imperial.  
+- The five day forecast displays the weather for 5 days in 3 hour increments.
+
+
+
+![image](https://user-images.githubusercontent.com/13501778/140249539-4ca815bf-9f4f-48ef-bfb3-2df884e6b745.png)
+
+
+
+- Weather.current holds the current weather data returned from [this call](https://openweathermap.org/current)
+- Weather.five_day holds the 5 day forecase in 3 hour increments from [five day](https://openweathermap.org/forecast5)
+- Location information gives the town name and state name
+- Wikipedia has the scrapped wikipage, an array of strings.
+
+
+## Packages used
+- [Axios](https://www.npmjs.com/package/axios)
+- [Express](https://www.npmjs.com/package/express)
+- [dotenv](https://www.npmjs.com/package/dotenv)
+- [html-entities](https://www.npmjs.com/package/html-entities)
+- [jssoup](https://www.npmjs.com/package/jssoup)
+- [nodemon](https://www.npmjs.com/package/nodemon)
+- [path](https://www.npmjs.com/package/path)
+- [ustates](https://www.npmjs.com/package/ustates)
---
+++ app.js
@@ -0,0 +1,146 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const states = require('ustates');
+const path = require('path');
+const JSSoup = require('jssoup').default;
+const axios = require('axios').default;
+const htmlEntities = require('html-entities');
+const dotEnv = require('dotenv').config();
+
+
+const app = express();
+
+const WEATHER_KEY = process.env.WEATHER_API_KEY;
+const ZIPCODE_VALIDATOR_URL = "http://localhost:5000/validate_zip?zipcode=";
+
+const PORT = 3000
+const PATH = path.join(__dirname);
+
+
+
+app.get("/", function(req, res){
+  res.send("Make a request to /location?zip=XXXX");
+});
+
+
+
+// Handler for a GET request
+app.get("/location", function(req, res){
+  // handles the get request for the location
+  // So, in order to scrape the wiki page, we need to convert the zip code to
+  // "town_name, state"
+
+  var Object_To_Send={
+    weather: {current:"",five_day:""},
+    location_info:{state:"", town:""},
+    wikipedia:[""]
+  };
+
+  var errObj={
+    message:"Error getting location data"
+  };
+
+  // validate the zipcode first
+  const check_zip = axios.get(ZIPCODE_VALIDATOR_URL+req.query.zip)
+  .then((response)=>{
+    if(response.data.valid_zip){
+      // get the weather data
+
+      let state_name = states[response.data.state].name;
+      let loc_name = [(response.data.city).replace(" ","_"), "_"+((state_name).replace(" ", "_")),response.data.city, state_name];
+
+      // setting up the url
+      wiki_url = "https://en.wikipedia.org/wiki/"+loc_name[0]+","+loc_name[1];
+      wiki_url = wiki_url.replace(' ',""); // 1 final check
+
+      const weather_url="https://api.openweathermap.org/data/2.5/weather?zip="+req.query.zip+"&units=imperial&appid="+WEATHER_KEY;
+      const weather_5_url="http://api.openweathermap.org/data/2.5/forecast?zip="+req.query.zip+"&units=imperial&appid="+WEATHER_KEY;
+
+      // get all 3 promises
+      const weather_current_request = axios.get(weather_url);
+      const req_wiki = axios.get(wiki_url);
+      const weather_5_day_request = axios.get(weather_5_url);
+
+      // on all the Promises.  These promises are in an aarray
+      // this is from the javascript documentation: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise/all
+      // axios will return a promise and then when we get them all we must wait for them to send the JSON object.
+      Promise.all([weather_current_request, req_wiki, weather_5_day_request])
+      .then((response)=>{
+        // all our promises came through.
+        // 0 index is current weather
+        Object_To_Send.weather.current = (response[0].data);
+
+        // 1 index is wikipedia
+        Object_To_Send.wikipedia = findWikiSection(response[1].data);
+        Object_To_Send.location_info.state = loc_name[3];
+        Object_To_Send.location_info.town = loc_name[2];
+
+        // 2nd index is 5 day
+        Object_To_Send.weather.five_day = (response[2].data);
+        res.json(Object_To_Send);
+      })
+      .catch(()=>{
+        // error with any of the promises, we need to handle that
+        console.error("Error getting the data for this location");
+        res.status(400).json(errObj);
+      });
+    }else{
+      console.error("Invalid zipcode");
+      res.status(400).json(errObj);
+    }
+
+  })
+  .catch(()=>{
+    console.error("Error validating zipcode");
+    res.status(400).json(errObj);
+  });
+
+});
+
+
+
+
+/*
+          FUNCTIONS FOR HANDLING DATA AND ZIPCODES.
+*/
+
+
+function findWikiSection(rawData){
+  //function that finds the wikipedia data sections and cleans it
+  //console.log(rawData);
+  var soup = new JSSoup(rawData);
+  var resultArr = [];
+  paragraphs = soup.findAll('p');
+
+  for(var i=0; i<paragraphs.length-1; i++){
+    // get the text for each paragraph element in the wikipedia page, and
+    // add that to a string, where each paragraph is a new element.
+    var temp = paragraphs[i].getText();
+    if(temp!=""){
+      resultArr.push(temp);
+    }
+  }
+  // add the results as an array
+  var merged = resultArr.toString();
+  // clean more of the output
+
+  merged = htmlEntities.decode(merged);
+
+  // reg expression that finds anything between the characters [ and ] with
+  // digits between 0-9, multiple
+  merged = merged.replace(/\[\d+\]/gm,"");
+
+  t = merged.split("\n");
+  for(var j=0; j<t.length; j++){
+    if(t[j][0] == ","){
+      t[j]= t[j].substring(1);
+    }
+  }
+  return t;
+};
+
+
+
+app.listen(PORT, function(){
+  console.log("Server started on port " + PORT);
+});
